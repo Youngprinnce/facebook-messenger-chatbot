900c125b5eddd4d12abb1848fd8f9b6431e41dfc
Ajiboye Adedotun <ajiboyeadedotun16@gmail.com>
2022-03-23 05:24:23 +0100

    configure routes and controller for chatbot

---
+++ src/api/controllers/chatbot.controller.js
@@ -0,0 +1,57 @@
+const { Chatbot } = require('../services/chatbot.service');
+
+const postWebhook = async (req, res) => {
+  try {
+    
+    
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getWebhook = async (req, res) => {
+  try {
+
+    
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const allMessages = async (req, res) => {
+  try {
+
+    
+    
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getSingleMessage = async (req, res) => {
+  try {
+
+    
+    
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const summary = async (req, res) => {
+  try {
+
+  
+    
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = {
+  postWebhook,
+  getWebhook,
+  allMessages,
+  getSingleMessage,
+  summary,
+}
---
+++ src/api/routes/chatbot.routes.js
@@ -0,0 +1,10 @@
+const chatBotRoute = require('../../config/routerConfig');
+const { postWebhook, getWebhook, allMessages, getSingleMessage, summary} = require('../controllers/chatbot.controller');
+
+chatBotRoute.post('/webhook', postWebhook);
+chatBotRoute.get('/webhook', getWebhook);
+chatBotRoute.get('/messages', allMessages);
+chatBotRoute.get('/messages/:id', getSingleMessage);
+chatBotRoute.get('/summary', summary);
+
+module.exports = chatBotRoute;
