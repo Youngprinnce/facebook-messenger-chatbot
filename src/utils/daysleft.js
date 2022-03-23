5ef5e8389063b22358dda81f7a439e39fe7ddc15
Ajiboye Adedotun <ajiboyeadedotun16@gmail.com>
2022-03-23 05:35:28 +0100

    add helper function

---
+++ src/utils/daysleft.js
@@ -0,0 +1,22 @@
+const moment = require('moment');
+
+const daysleft = (birthday) => {
+    let bday = moment(birthday).format('YYYY-MM-DD');
+    let now = moment().format('YYYY-MM-DD');
+    bdayNow = bday.split('-')
+    bdayNow = `${now.split('-')[0]}-${bdayNow[1]}-${bdayNow[2]}`
+    bdayNow = moment(bdayNow).format('YYYY-MM-DD')
+    let count;
+    if(bdayNow > now){
+        count = moment(bdayNow).diff(moment(now), 'days')
+    } else if(bdayNow < now) {
+        bdayNow = moment(bdayNow).add(1, 'year').format('YYYY-MM-DD')
+        bdayNow = moment(bdayNow).format('YYYY-MM-DD')
+        count = moment(bdayNow).diff(moment(now), 'days')
+    } else {
+        count = 0
+    }
+    return count;
+}
+
+module.exports = daysleft;
